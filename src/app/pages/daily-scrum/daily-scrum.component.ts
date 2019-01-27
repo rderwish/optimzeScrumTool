bf6ad58be73e03b3cdbf790c6e0234a7322c4eb9
Renas Derwish <renas.derwish@hotmail.de>
2019-01-27 17:05:40 +0100

    daily page completed

--- src/app/pages/daily-scrum/daily-scrum.component.ts
+++ src/app/pages/daily-scrum/daily-scrum.component.ts
@@ -8,43 +8,51 @@ import { interval, Observable, timer } from "rxjs";
   styleUrls: ["./daily-scrum.component.scss"]
 })
 export class DailyScrumComponent implements OnInit {
+  // 
   public numberOfDailyMember: number = 0;
+  //
   public counter: number = 0;
+  //
   public numberOfSelectedMember: number = 0;
+  //
   public dailyDoneMessage: boolean = false;
+  //
   public talkedTimeTalk: number = 0;
+  //
   public onDailyTimer: boolean = false;
-
+  //
   public mode = "determinate";
+
   constructor() {}
 
   ngOnInit() {}
 
+  /**
+   *
+   * @param dailyMember
+   */
   startDailyTimer(dailyMember: number) {
     this.numberOfDailyMember = dailyMember;
     this.onDailyTimer = true;
     this.activateTimer(dailyMember);
-    
-
-    
   }
+
+  /**
+   *
+   * @param dailyMember
+   */
   activateTimer(dailyMember) {
     setInterval(() => {
-      if (
-        this.talkedTimeTalk < 180 &&
-        (this.numberOfSelectedMember+1) <= dailyMember
-      ) {
+      if (this.talkedTimeTalk < 180 && this.numberOfSelectedMember + 1 <= dailyMember) {
         this.talkedTimeTalk++;
         this.counter = this.counter + 10;
-        console.log("counter", this.counter);
-        console.log("talkedTimetalk", this.talkedTimeTalk);
         if (this.counter > 99) {
           this.numberOfSelectedMember++;
           this.talkedTimeTalk = 0;
           this.counter = 0;
         }
         if (this.numberOfSelectedMember >= dailyMember) {
-          this.onDailyTimer=!false;
+          this.onDailyTimer = !false;
           this.dailyDoneMessage = !this.dailyDoneMessage;
         }
       }
